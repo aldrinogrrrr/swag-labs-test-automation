062f0c0d96092b9d66355d6d1b5e11d55d17da63
Aldrin <137375592+aldrinogrrrr@users.noreply.github.com>
2026-07-22 21:43:16 +0800

    feat: add navigation and validation tests related to inventory page

---
+++ locators/inventoryPageLocators.js
@@ -0,0 +1,7 @@
+export const inventoryLocators = {
+    inventoryName: ".inventory_details_name",
+    inventoryPrice: ".inventory_details_price",
+
+    //button
+    addToCartBtn: "#add-to-cart"
+}
---
+++ tests/inventorypage.view.spec.js
@@ -0,0 +1,31 @@
+const { test } = require('@playwright/test');
+require('dotenv').config();
+const LoginPage = require('../pages/loginPage');
+const HomePage = require('../pages/homePage');
+const InventoryPage = require('../pages/inventoryPage');
+const { orderData } = require('../test-data/order-test-data');
+
+
+test.describe('Validate Inventory Page', () => {
+
+    let loginPage;
+    let homePage;
+    let inventoryPage;
+
+    test.beforeEach(async ({page}) => {
+        loginPage = new LoginPage(page);
+        homePage = new HomePage(page);
+        inventoryPage = new InventoryPage(page);
+        await loginPage.navigateToLoginPage(process.env.baseURL);
+        await loginPage.login(process.env.user, process.env.password);
+    });
+
+    test('TC009 [Inventory Page] View Product In Inventory Page', async () => {
+        await inventoryPage.navigateToInventoryPage(orderData.firstOrder);
+        await inventoryPage.validateProductInventory(orderData.firstOrder);
+    });
+
+
+
+
+});
